f457726eb95d86d05196766bdfb75c903c05a92b
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2023-04-26 00:31:23 -0500

    Navigation bar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Nav from './_components/_nav/_nav.svelte';
+</script>
+<Nav/>
+<slot />
---
+++ src/routes/_components/_nav/_nav.svelte
@@ -0,0 +1,76 @@
+<script>
+    import {page} from "$app/stores";
+
+</script>
+<style>
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0;
+        margin-top: 0;
+        margin-right: 1.1em;
+        margin-left: 0.6em;
+    }
+
+    .left a{
+        display: flex;
+    }
+    .left img {
+        width: 20em;
+    }
+    a {
+        padding: 0.9em;
+        color: black;
+        align-items: center;
+    }
+
+    .right a, a:visited {
+        min-width: 1em;
+        text-align: center;
+        font-family: 'Lato';
+        font-weight: 500;
+        text-decoration: none;
+        font-size: 18px;
+        margin: 0.2em;
+    }
+
+    .right a:hover:not(:last-child) {
+        border-radius: 10px;
+        background-color: #FF7F50;
+        color: white;
+    }
+
+    .active {
+        border-radius: 10px;
+        background-color: #FF7F50;
+        color: white;
+    }
+
+    .right{
+        display: flex;
+        flex-direction: row;
+    }
+
+    .linkedin{
+        display: flex;
+        padding: 0.75em;
+    }
+    .linkedin img{
+        width: 30px;
+    }
+
+</style>
+<header>
+    <nav>
+        <div class="left">
+            <a href="/"><img src="/nav_logo.jpeg" alt="NF Manufacturing & Design"></a>
+        </div>
+        <div class="right">
+            <a class:active={$page.route.id === "/capabilities"} href="/capabilities">Capabilities</a>
+            <a class:active={$page.route.id === "/about"} href="/about">About</a>
+            <a class:active={$page.route.id === "/contact"} href="/contact">Contact</a>
+            <a class="linkedin" target="_blank" href="https://www.linkedin.com/company/nf-maufacturing-design/"><img src="linkedin.png" alt=""></a>
+        </div>
+    </nav>
+</header>
